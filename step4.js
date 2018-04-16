82bb4d79286dbf65cd7b380512bb04bf3c3c1c94
corrinachow <corrina.yy.chow@gmail.com>
2018-04-16 12:45:26 -0400

    Separate getHTML and printHTML functions

---
+++ step4.js
@@ -0,0 +1,37 @@
+var https = require('https');
+
+function getHTML (options, callback) {
+    https.get(options, function (response) {
+
+    let output = ''
+
+    response.setEncoding('utf8');
+    response.on('error', function() {
+      console.log(`There's an error`);
+    });
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      output += data;
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log('Response stream complete.');
+      callback(output);
+    });
+  });
+}
+
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+
+getHTML(requestOptions, printHTML);
